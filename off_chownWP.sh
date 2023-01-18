42211754ac8f7a9deab3a7f39718bbc7952dfba3
JamesOffice <james@u-sen.com.tw>
2023-01-18 12:41:47 +0800

    update off_chownWP.sh

--- off_chownWP.sh
+++ off_chownWP.sh
@@ -2,5 +2,5 @@
 sudo chown -R www-data:www-data /var/www/$1/
 echo '\n'
 echo '目錄權限已設定為可直接後台安裝外掛\n'
-echo '安裝完畢後請再執行 initWP.sh 以保持 WordPress 較安全的權限設定'
+echo '安裝完畢後請再執行 on_chownWP.sh 以保持 WordPress 較安全的權限設定'
 echo '\n'
