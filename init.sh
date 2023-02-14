14ba891aecaf6e16f131b67eeae2d2d3455097ba
JamesNB <james@u-sen.com.tw>
2023-02-14 17:51:05 +0800

    init.sh crontab.txt

--- init.sh
+++ init.sh
@@ -17,47 +17,6 @@ sudo systemctl enable --now nginx
 sudo systemctl status nginx
 sudo sleep 3s
 
-# Install Rsync Server
-# sudo apt install -y rsync
-# sudo mkdir /var/log/rsync
-# sudo cat << EOF > rsyncd.conf
-# uid = $USER     
-# gid = $USER    
-# use chroot = yes   
-# max connections = 4   
-# log file=/var/log/rsync/rsyncd.log  
-# pid file = /var/run/rsyncd.pid  
-# exclude = lost+found/  
-# transfer logging = yes   
-# timeout = 900    
-# ignore nonreadable = yes   
-# dont compress   = *.gz *.tgz *.zip *.z *.Z *.rpm *.deb *.bz2  
-# [$1] 
-# 	path = /var/www/$1    
-#     comment = $1 website backup    
-#     read only = no     
-#     auth users = $USER    
-#     secrets file = /home/$USER/rsync.secret   
-#     log file = /var/log/rsync/$1.log
-# [phpMyAdmin]
-#     path = /var/www/phpMyAdmin    
-#     comment = phpMyAdmin backup    
-#     read only = no     
-#     auth users = $USER    
-#     secrets file = /home/$USER/rsync.secret   
-#     log file = /var/log/rsync/phpMyAdmin.log    
-# EOF
-# sudo mv rsyncd.conf /etc/rsyncd.conf
-# sudo chown $USER:$USER /etc/rsyncd.conf
-# sudo cat << EOF > rsync.secret
-# $USER:your passwd
-# EOF
-# sudo mv rsync.secret /home/$USER/rsync.secret
-# sudo chmod 600 /home/$USER/rsync.secret
-# sudo chown root:root /home/$USER/rsync.secret
-# sudo systemctl start rsync
-# sudo systemctl enable rsync
-
 # firewall 設定
 sudo service ufw start
 sudo service ufw enable
