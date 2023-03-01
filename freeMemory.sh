28bce540191a71809b4d5a1c1999da1b879bbe75
JamesOffice <james@u-sen.com.tw>
2023-03-01 17:10:02 +0800

    freeMemory.sh 新增註解磁碟維護

--- freeMemory.sh
+++ freeMemory.sh
@@ -9,6 +9,7 @@ sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'
 sudo systemctl restart php8.1-fpm.service
 sudo journalctl --vacuum-time=3d
 sudo apt autoremove
+sudo apt autoremove --purge
 sudo apt autoclean
 sudo apt clean
 sudo apt list --upgradable
@@ -16,3 +17,22 @@ echo "=========記憶體資訊========="
 sudo free -m
 echo "=========磁碟空間==========="
 sudo df -h
+# 查詢已安裝的套件
+# sudo dpkg --get-selections
+
+# 擴充磁碟
+# sudo df -h        # 顯示磁碟各磁區容量
+# sudo vgdisplay    # 顯示磁碟狀況 Free PE 為可用容量
+
+# 100% 全部給
+# sudo lvextend -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
+
+# 給 50G     
+# sudo lvextend -L 50G /dev/mapper/ubuntu--vg-ubuntu--lv
+
+# 重新計算磁區
+# sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv 
+
+# 重新檢視磁碟
+# df -h
+# sudo vgdisplay
